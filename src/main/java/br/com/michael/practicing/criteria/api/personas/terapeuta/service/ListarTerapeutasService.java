aba97a301fa617c9c957048ecf2539d419c61252
Michael Diogo Fagundes <michael@petize.com.br>
2022-11-05 14:38:23 -0300

    feat: adicinado filtros por  cpf,  email,  login,  especializacao e  idsTerapeutas

--- src/main/java/br/com/michael/practicing/criteria/api/personas/terapeuta/service/ListarTerapeutasService.java
+++ src/main/java/br/com/michael/practicing/criteria/api/personas/terapeuta/service/ListarTerapeutasService.java
@@ -16,7 +16,7 @@ public class ListarTerapeutasService {
 	private final TerapeutaRepository terapeutaRepository;
 	private final EntityManager entityManager;
 
-	public List<Terapeuta> execute( String nome){
+	public List<Terapeuta> execute( String nome, String cpf, String email, String login, String especializacao, List<Long> idsTerapeutas){
 
 		String jpql = "SELECT t FROM Terapeuta t " +
 		              "JOIN DadosPessoais d on d = t.dadosPessoais " +
@@ -27,10 +27,51 @@ public class ListarTerapeutasService {
 			jpql += " AND lower(d.nomeCompleto) LIKE lower(CONCAT('%',:nome,'%')) ";
 		}
 
+		if(cpf != null && !cpf.trim().isEmpty()){
+			jpql += " AND d.cpf LIKE CONCAT('%',:cpf,'%') ";
+		}
+
+		if(email != null && !email.trim().isEmpty()){
+			jpql += " AND lower(d.email) LIKE lower(CONCAT('%',:email,'%')) ";
+		}
+
+		if(login != null && !login.trim().isEmpty()){
+			jpql += " AND u.login LIKE CONCAT('%',:login,'%') ";
+		}
+
+		if(especializacao != null && !especializacao.trim().isEmpty()){
+			jpql += " AND lower(t.especializacao) LIKE lower(CONCAT('%',:especializacao,'%')) ";
+		}
+
+		if (!idsTerapeutas.isEmpty()){
+			jpql += " AND t.id IN :idsTerapeutas ";
+		}
+
+
 		TypedQuery<Terapeuta> query = entityManager.createQuery(jpql, Terapeuta.class);
 
 		if(nome != null && !nome.trim().isEmpty()){
 			query.setParameter("nome", nome);
+		}
+
+		if(cpf != null && !cpf.trim().isEmpty()){
+			query.setParameter("cpf", cpf);
+		}
+
+		if(email != null && !email.trim().isEmpty()){
+			query.setParameter("email", email);
+		}
+
+		if(login != null && !login.trim().isEmpty()){
+			query.setParameter("login", login);
+		}
+
+		if(especializacao != null && !especializacao.trim().isEmpty()){
+			query.setParameter("especializacao", especializacao);
+		}
+
+		if(!idsTerapeutas.isEmpty()){
+			query.setParameter("idsTerapeutas", idsTerapeutas);
 		}
 
 		return query.getResultList();

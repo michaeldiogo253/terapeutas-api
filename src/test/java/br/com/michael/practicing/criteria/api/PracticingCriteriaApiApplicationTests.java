a5ee7a6b6984fa67aeab51ca3d5ee8f49f40022c
Michael Diogo Fagundes <michael@petize.com.br>
2022-11-05 13:51:19 -0300

    feat: init commit

---
+++ src/test/java/br/com/michael/practicing/criteria/api/PracticingCriteriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.michael.practicing.criteria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingCriteriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

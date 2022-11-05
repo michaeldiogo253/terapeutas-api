e87118e8bd52ddd7753fa1aa2a8a97848044ba0a
Michael Diogo Fagundes <michael@petize.com.br>
2022-11-05 13:49:38 -0300

    feat: criacao de classe Terapeuta

---
+++ src/main/java/br/com/michael/practicing/criteria/api/personas/terapeuta/domain/Terapeuta.java
@@ -0,0 +1,24 @@
+package br.com.michael.practicing.criteria.api.personas.terapeuta.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Terapeuta {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) private long id;
+	@OneToOne(cascade = CascadeType.ALL) private DadosPessoais dadosPessoais;
+	private String numeroRegistro;
+	private String especializacao;
+
+	public Terapeuta(DadosPessoais dadosPessoais, String numeroRegistro, String especializacao) {
+
+		this.dadosPessoais = dadosPessoais;
+		this.numeroRegistro = numeroRegistro;
+		this.especializacao = especializacao;
+	}
+}
